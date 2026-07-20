64f398b23b6ceb6a0b9fc4503654d2b6838fa131
tlkcexe <aristotelischatzivasileiou@gmail.com>
2026-07-20 20:46:29 +0300

    Fix file size number

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cors = require('cors');
 require('dotenv').config();
 const multer = require('multer');
 
-// Ρύθμιση του multer (τα αρχεία αποθηκεύονται προσωρινά στη μνήμη ή σε φάκελο uploads)
+// Ρύθμιση του multer για τα uploads
 const upload = multer({ dest: 'uploads/' });
 
 var app = express();
@@ -15,17 +15,16 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// POST endpoint για το ανέβαστη αρχείου (το input πρέπει να ονομάζεται "upfile")
+// Τελικό endpoint για το File Metadata
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
   if (!req.file) {
     return res.json({ error: 'No file uploaded' });
   }
 
-  // Επιστρέφει τα ζητούμενα metadata: όνομα, τύπο και μέγεθος
   res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
-    size: req.file.size
+    size: Number(req.file.size)
   });
 });
 
